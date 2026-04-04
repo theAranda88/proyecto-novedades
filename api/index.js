21da9623b8d06924df45ebf4ac4ce1c0179f3982
Daniel Aranda <thearanda88@gmail.com>
2026-04-04 18:39:33 -0500

    Fix: Corregir ruta de módulo para Vercel (../ → ./)

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@
 // Handler serverless para Vercel
 // Importa y exporta la aplicación Express compilada
 
-const appModule = require('../dist/src/app');
+const appModule = require('./dist/src/app');
 const app = appModule.default || appModule;
 
 module.exports = app;
+
 
 
 
