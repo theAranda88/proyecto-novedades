6f04c7f60641ec6b13f8895a680c00918e8c272d
Daniel Aranda <thearanda88@gmail.com>
2026-04-04 19:08:24 -0500

    Fix: Resolver Function Runtime error - usar index.ts en raíz

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-// api/index.ts
-// Vercel serverless function handler
-import app from '../src/app';
-
-export default app;
-
---
+++ index.ts
@@ -0,0 +1,7 @@
+// index.ts (raíz del proyecto)
+// Punto de entrada único para Vercel
+
+import app from './src/app';
+
+export default app;
+
